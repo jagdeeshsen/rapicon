aa535411ab9c69f1460d62b881f794804de2833c
git <jagdeeshsen35@gmail.com>
2025-10-05 16:24:20 +0530

    update register endpoint

--- rapicon/src/main/resources/static/scripts/register.js
+++ rapicon/src/main/resources/static/scripts/register.js
@@ -131,7 +131,7 @@
         }*/
 
         try {
-            const response = await fetch('http://localhost:8080/api/auth/register', {
+            const response = await fetch('/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
